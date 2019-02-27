26bfe6670916e62b0aac07c0ed6fca4dbbf1105d
yaoleia <yaoleia@163.com>
2019-02-27 17:45:59 +0800

    some demos

--- src/demo2.js
+++ src/demo2.js
@@ -1,16 +1,30 @@
+'use strict';
 const devices = require('puppeteer/DeviceDescriptors')
 const puppeteer = require('puppeteer');
 
 (async () => {
     const browser = await puppeteer.launch({
-        headless: true
+        headless: false,
+        ignoreHTTPSErrors: true,
     })
+    try {
+        const page = await browser.newPage()
+        // await page.emulate(devices['iPhone X'])
+        await page.goto('http://www.baidu.com')
+        await page.type('#kw', '韦德')
+        await page.click('#su')
 
-    const page = await browser.newPage()
-    await page.emulate(devices['iPhone X'])
+        // await page.waitForNavigation({ timeout: 5000 })
 
-    await page.goto('http://www.baidu.com')
-    await page.screenshot({
-        path: 'c:/temp/baidu_iphone_X.png'
-    })
+        await page.waitFor(2000);
+
+        await page.screenshot({
+            path: '/Users/admin/Documents/screenshot/baidu_iphone_X.png',
+            fullPage: true
+        })
+    } catch (error) {
+        console.error(error)
+    } finally {
+        await browser.close();
+    }
 })()
--- src/demo3.js
+++ src/demo3.js
@@ -1,20 +1,33 @@
 const devices = require('puppeteer/DeviceDescriptors')
 const puppeteer = require('puppeteer');
-
 (async () => {
     const browser = await puppeteer.launch({
         headless: true
     })
 
-    const page = await browser.newPage()
-    await page.emulate(devices['iPhone X'])
+    try {
+        const page = await browser.newPage()
+        // await page.emulate(devices['iPhone X'])
 
-    await page.goto('http://www.baidu.com')
-    await page.type('#index-kw', 'puppeteer')
-    await page.click('#index-bn')
-    await page.waitForNavigation({ timeout: 3000 })
+        await page.goto('http://lh.nemoface.com/')
+        await page.type('.el-input__inner[type="text"]', 'admin')
+        await page.type('.el-input__inner[type="password"]', 'deepglint')
+        await page.click('.el-button[type="button"]')
+        // const elementHandle = await page.$('.el-input__inner[type="password"]');
+        // await elementHandle.press('Enter');
+        await page.setViewport({ width: 1920, height: 1080 });
 
-    await page.screenshot({
-        path: 'c:/temp/baidu_iphone_X_search_puppeteer.png'
-    })
+        // await page.waitForNavigation({ timeout: 3000 })
+
+        await page.waitFor(5000);
+
+        await page.screenshot({
+            path: '/Users/admin/Documents/screenshot/baidu_iphone_X_search_puppeteer.png'
+        })
+    } catch (error) {
+        console.error(error)
+    } finally {
+        await browser.close()
+    }
+
 })()
