e2bdb60e52d16a41e50ba62336622a99b05f3932
yaoleia <yaoleia@163.com>
2019-02-28 11:33:02 +0800

    demo4 iframe-evaluate musicName

--- src/demo4.js
+++ src/demo4.js
@@ -3,14 +3,14 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     const browser = await (puppeteer.launch({ headless: false }));
-    const musicName = '光年之外';
+    const musicName = '时间是金';
 
     try {
         const page = await browser.newPage();
         // 进入页面
         await page.goto('https://music.163.com/#');
 
-        // 点击搜索框拟人输入 时间是金
+        // 点击搜索框拟人输入 musicName
         await page.type('.txt.j-flag', musicName);
 
         // 回车
@@ -21,18 +21,24 @@ const puppeteer = require('puppeteer');
 
         let iframe = await page.frames().find(f => f.name() === 'contentFrame');
         const SONG_LS_SELECTOR = await iframe.$('.srchsongst');
-        // 获取歌曲 时间是金 的地址
-        const selectedSongHref = await iframe.evaluate(e => {
+
+        // 获取歌曲 musicName 的列表信息
+        const songList = await iframe.evaluate(e => {
             const songList = Array.from(e.childNodes);
-            const idx = songList.findIndex(v => {
-                const text = v.childNodes[1].innerText;
-                return text.indexOf('光年之外') !== -1;
+            return songList.map(v => {
+                if (v.querySelector('.s-fc7')) {
+                    return {
+                        title: v.querySelector('.s-fc7').innerText + v.childNodes[1].querySelector('[title]').title,
+                        href: v.childNodes[1].querySelector('a').href,
+                        singer: v.childNodes[3].querySelector('a').innerText
+                    };
+                }
             });
-            return songList[idx].childNodes[1].firstChild.firstChild.firstChild.href;
         }, SONG_LS_SELECTOR);
 
         // 进入歌曲页面
-        await page.goto(selectedSongHref);
+        const selected = songList.find(s => s.title.indexOf(musicName) != -1);
+        await page.goto(selected.href);
 
         // 获取歌曲页面嵌套的 iframe
         await page.waitFor(2000);
