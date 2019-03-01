44ac9d075f00a6f0e11e3f481a77af65b7f72f4d
yaoleia <yaoleia@163.com>
2019-03-01 17:24:12 +0800

    demo4

--- src/demo4.js
+++ src/demo4.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 
-(async () => {
+async function downMusic(music) {
     const browser = await (puppeteer.launch({ headless: false }));
-    const musicName = '时间是金';
-
+    const search = music.split(" ");
+    const [songName, singer] = search;
     try {
         const page = await browser.newPage();
         // 进入页面
         await page.goto('https://music.163.com/#');
 
-        // 点击搜索框拟人输入 musicName
-        await page.type('.txt.j-flag', musicName);
+        // 点击搜索框拟人输入 songName
+        await page.type('.txt.j-flag', music);
 
         // 回车
         await page.keyboard.press('Enter');
@@ -22,26 +22,32 @@ const puppeteer = require('puppeteer');
         let iframe = await page.frames().find(f => f.name() === 'contentFrame');
         const SONG_LS_SELECTOR = await iframe.$('.srchsongst');
 
-        // 获取歌曲 musicName 的列表信息
-        const songList = await iframe.evaluate(e => {
+        // 获取歌曲 songName 的列表信息
+        let songList = await iframe.evaluate(e => {
             const songList = Array.from(e.childNodes);
             return songList.map(v => {
-                if (v.querySelector('.s-fc7')) {
-                    return {
-                        title: v.querySelector('.s-fc7').innerText + v.childNodes[1].querySelector('[title]').title,
-                        href: v.childNodes[1].querySelector('a').href,
-                        singer: v.childNodes[3].querySelector('a').innerText
-                    };
-                }
+                return {
+                    title: v.childNodes[1].querySelector('[title]').title,
+                    href: v.childNodes[1].querySelector('a').href,
+                    singer: v.childNodes[3].innerText
+                };
             });
         }, SONG_LS_SELECTOR);
+        // console.log(songList)
 
         // 进入歌曲页面
-        const selected = songList.find(s => s.title.indexOf(musicName) != -1);
-        await page.goto(selected.href);
+        songList = songList.filter(s => !!s);
+        const selected = songList.find(s => {
+            if (!singer) {
+                return s.title.indexOf(songName) != -1
+            }
+            return s.singer.indexOf(singer) != -1 && s.title.indexOf(songName) != -1
+        });
+
+        await page.goto(selected ? selected.href : songList[0].href);
 
         // 获取歌曲页面嵌套的 iframe
-        await page.waitFor(2000);
+        await page.waitFor(3000);
         iframe = await page.frames().find(f => f.name() === 'contentFrame');
 
         // 点击 展开按钮
@@ -54,22 +60,22 @@ const puppeteer = require('puppeteer');
             return e.innerText;
         }, LYRIC_SELECTOR);
 
-        console.log(lyricCtn);
+        // console.log(lyricCtn);
 
         // 截图
         await page.screenshot({
-            path: '歌曲.png',
+            path: `${songName}.png`,
             fullPage: true,
         });
 
         // 写入文件
-        let writerStream = fs.createWriteStream('歌词.txt');
+        let writerStream = fs.createWriteStream(`${songName}.txt`);
         writerStream.write(lyricCtn, 'UTF8');
         writerStream.end();
 
         // 获取评论数量
         const commentCount = await iframe.$eval('.sub.s-fc3', e => e.innerText);
-        console.log(commentCount);
+        // console.log(commentCount);
 
         // 获取评论
         const commentList = await iframe.$$eval('.itm', elements => {
@@ -78,10 +84,14 @@ const puppeteer = require('puppeteer');
             });
             return ctn;
         });
-        console.log(commentList);
+        // console.log(commentList);
     } catch (error) {
         console.error(error)
     } finally {
         await browser.close()
     }
-})();
+}
+
+const list = ["时间是金 王以太", "光年之外"]
+
+list.forEach(li => downMusic(li))
